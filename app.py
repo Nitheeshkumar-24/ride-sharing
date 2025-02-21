d16cc682c53be24005a5d952730c142c5fb49756
Nitheeshkumar-24 <Nitheeshkumar-24>
2025-02-21 23:21:45 +0530

    db connection estalished with  an example

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
+import firebase_admin
+from firebase_admin import credentials, firestore
 
 app = Flask(__name__)
 
+# Initialize Firebase FIRST
+cred = credentials.Certificate(r"E:\git files\ride-sharing\ride-sharing-b7053-firebase-adminsdk-fbsvc-6b3474ac7d.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+# Flask Routes
 @app.route('/')
 def authentication():
     return render_template('authentication.html')
@@ -9,7 +17,11 @@ def authentication():
 @app.route('/index')
 def index():
     return render_template('index.html')
-        
+
+# Store data when the app starts
+doc_ref = db.collection("users").document("alovelace")
+doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})
+print("Data stored successfully!")
 
 if __name__ == '__main__':
     app.run(debug=True)
